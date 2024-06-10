e858fbe8d94a649bdf66d91adac1b428a1666a03
BxsedSosa <donovanxabad@gmail.com>
2024-06-10 10:21:51 -0700

    completed player to ball collision now working on cpu basic ai to get to the ball

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys, pygame
+from time import sleep
+
+from pygame.cursors import ball
 
 pygame.init()
 
@@ -7,8 +10,8 @@ screen = pygame.display.set_mode(size=window)
 clock = pygame.time.Clock()
 fps = 0
 
-x = 6
-y = 6
+x = 5
+y = 5
 ball_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 player_pos = pygame.Rect(20, screen.get_height() / 2, 10, 70)
 cpu_pos = pygame.Rect(screen.get_width() - 30, screen.get_height() / 2, 10, 70)
@@ -17,17 +20,38 @@ cpu_pos = pygame.Rect(screen.get_width() - 30, screen.get_height() / 2, 10, 70)
 def movement(object_position):
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        object_position[1] -= 6
+        object_position[1] -= 5
     if keys[pygame.K_DOWN]:
-        object_position[1] += 6
+        object_position[1] += 5
 
 
 def check_ball_collision(ball_pos):
     global x, y
     if ball_pos[1] > window[1]:
-        y = -6
+        y *= -1
     if ball_pos[1] < 0:
-        y = 6
+        y *= -1
+
+
+def check_for_winner():
+    global x, y, ball_pos
+
+    if ball_pos[0] > window[0]:
+        x = -5
+        y = 5
+        ball_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    elif ball_pos[0] < 0:
+        x = 5
+        y = 5
+        ball_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+
+def bar_ball_collision(ball_pos, object_position):
+    global x
+    if ball_pos[0] < object_position[0] and ball_pos[1] in range(
+        object_position[1], object_position[1] + object_position[3]
+    ):
+        x *= -1
 
 
 while True:
@@ -36,7 +60,9 @@ while True:
             print("Exit")
             sys.exit()
 
-    circle = pygame.draw.circle(screen, "white", ball_pos, 10)
+    print(player_pos)
+
+    circle = pygame.draw.circle(screen, "white", ball_pos, 5)
     player = pygame.draw.rect(screen, "white", player_pos)
     cpu = pygame.draw.rect(screen, "white", cpu_pos)
 
@@ -45,6 +71,8 @@ while True:
 
     check_ball_collision(ball_pos)
     movement(player_pos)
+    bar_ball_collision(ball_pos, player_pos)
+    check_for_winner()
 
     pygame.display.flip()
     screen.fill("black")
